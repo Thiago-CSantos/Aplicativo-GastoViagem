abb3cb6c3dde4ee0ed6e2bc80bbe70d86fd224bf
Thiago C. Santos <thiagocarretero433@gmail.com>
2023-01-14 21:31:22 -0300

    Adicionando o evento de click com View.OnClickListener

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,7 +15,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonCalculate.setOnClickListener(this)
 
+    }
 
+    override fun onClick(click: View) {
+        if (click.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun calculate(): Unit {
+        Toast.makeText(applicationContext, "Fui clicado!", Toast.LENGTH_LONG).show()
     }
 }
